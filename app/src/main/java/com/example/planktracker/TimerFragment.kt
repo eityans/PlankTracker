ef7d15713b5028f130a2c96b51467ebeb90dc501
eityans <eityans@gmail.com>
2021-08-22 02:22:31 +0900

    imp creating plank

--- app/src/main/java/com/example/planktracker/TimerFragment.kt
+++ app/src/main/java/com/example/planktracker/TimerFragment.kt
@@ -1,59 +1,76 @@
 package com.example.planktracker
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.planktracker.databinding.FragmentTimerBinding
+import com.google.android.material.snackbar.Snackbar
+import io.realm.Realm
+import io.realm.kotlin.createObject
+import io.realm.kotlin.where
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TimerFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TimerFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentTimerBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var realm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        realm = Realm.getDefaultInstance()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timer, container, false)
+        _binding = FragmentTimerBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TimerFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TimerFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as? MainActivity)?.setFabVisible(View.INVISIBLE)
+        binding.save.setOnClickListener { savePlank(it) }
     }
+
+    private fun savePlank(view: View) {
+        realm.executeTransaction { db: Realm ->
+            val maxId = db.where<Plank>().max("id")
+            val nextId = (maxId?.toLong() ?: 0L) + 1L
+            val plank = db.createObject<Plank>(nextId)
+            val date = Date(System.currentTimeMillis())
+            plank.date = date
+            plank.sec = Integer.parseInt(binding.secEdit.text.toString())
+        }
+        Snackbar.make(view, "追加しました", Snackbar.LENGTH_SHORT)
+            .setAction("戻る") { findNavController().popBackStack() }
+            .setActionTextColor(Color.YELLOW)
+            .show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
+    }
+
+//    private fun getNowDate(): String {
+//        val df: DateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm")
+//        val date = Date(System.currentTimeMillis())
+//        return df.format(date)
+//    }
+
+
 }
