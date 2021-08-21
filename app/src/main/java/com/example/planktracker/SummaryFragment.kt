25b64413102a5b5476abdc57abb3edfb396e573c
eityans <eityans@gmail.com>
2021-08-21 23:09:44 +0900

    rename

--- app/src/main/java/com/example/planktracker/SummaryFragment.kt
+++ app/src/main/java/com/example/planktracker/SummaryFragment.kt
@@ -1,24 +1,26 @@
 package com.example.planktracker
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class SummaryFragment : Fragment() {
+
+
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_summary, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
